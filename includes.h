e7aeb00f0e18cfb25d45f5f64a227f362fda9eff
Patrik Görling <p.gorling@gmail.com>
2024-01-05 18:31:08 +0100

    created a general include file

---
+++ includes.h
@@ -0,0 +1,6 @@
+#include "keys_sv.h"
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/outputs.h>
+#include <dt-bindings/zmk/mouse.h>
