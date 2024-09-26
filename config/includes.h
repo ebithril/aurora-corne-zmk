0cfb4145b3c11cf902e96e110c8726a65382ff04
Patrik Görling <patrik.gorling@ringtailinteractive.com>
2024-09-26 13:03:31 +0200

    add an include to maybe fix compile issues

--- config/includes.h
+++ config/includes.h
@@ -3,4 +3,6 @@
 #include <dt-bindings/zmk/keys.h>
 #include <dt-bindings/zmk/bt.h>
 #include <dt-bindings/zmk/outputs.h>
+
+#include <zephyr/dt-bindings/input/input-event-codes.h> // seems to be needed for mouse
 #include <dt-bindings/zmk/mouse.h>
